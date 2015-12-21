b92b136a3c4b69b186c0cc8e02e88761ef4cf254
Brian Jacobel <bjacobel@gmail.com>
2015-12-21 12:44:03 -0500

    Passing tests for github services, holla

--- test/services/github.test.js
+++ test/services/github.test.js
@@ -4,21 +4,26 @@ require('isomorphic-fetch');
 
 describe('Github services', () => {
   let sandbox;
+  let server;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
+    server = sinon.fakeServer.create();
+    server.respondImmediately = true;
   });
 
   afterEach(() => {
     sandbox.restore();
+    server.restore();
   });
 
   describe('getActivity', () => {
-    let mockStorage = {};
+    let mockStorage;
     let githubURL;
-    let fetchSpy;
+    let fetchStub;
 
     beforeEach(() => {
+      mockStorage = {};
       global.localStorage = {};
       global.localStorage.getItem = (itemName) => {
         if (mockStorage.hasOwnProperty(itemName)) {
@@ -28,38 +33,106 @@ describe('Github services', () => {
       };
 
       global.localStorage.setItem = (itemName, itemVal) => {
-        mockStorage = Object.assign(mockStorage, { itemName: itemVal });
+        mockStorage = Object.assign(mockStorage, { [itemName]: itemVal });
       };
 
-      fetchSpy = sandbox.spy(global, 'fetch');
+      fetchStub = sandbox.stub(global, 'fetch');
 
       githubURL = `https://api.github.com/users/${GITHUB.USER}/events`;
     });
 
-    it('sends params with just User-Agent if no etag in localstorage', () => {
-      getActivity();
+    describe('while sending', () => {
+      beforeEach(() => {
+        fetchStub.resolves();
+      });
 
-      expect(fetchSpy).to.have.been.calledOnce;
-      expect(fetchSpy).to.have.been.calledWith(githubURL, {
-        headers: {
-          'User-Agent': GITHUB.UA
-        }
+      it('sends params with just User-Agent if no etag in localstorage', () => {
+        getActivity();
+
+        expect(fetchStub).to.have.been.calledOnce;
+        expect(fetchStub).to.have.been.calledWith(githubURL, {
+          headers: {
+            'User-Agent': GITHUB.UA
+          }
+        });
+      });
+
+      it('sets an etag header if one exists in localstorage', () => {
+        Object.assign(mockStorage, {
+          [GITHUB.LOCALSTORAGE_ETAG_KEY]: 'asdf'
+        });
+
+        getActivity();
+
+        expect(fetchStub).to.have.been.calledOnce;
+        expect(fetchStub).to.have.been.calledWith(githubURL, {
+          headers: {
+            'User-Agent': GITHUB.UA,
+            ETag: 'asdf'
+          }
+        });
       });
     });
 
-    it('sets an etag header if one exists in localstorage', () => {
-      Object.assign(mockStorage, {
-        [GITHUB.LOCALSTORAGE_ETAG_KEY]: 'asdf'
+    describe('while recieving', () => {
+      it('returns whatever what was memoized if fetch is passed an etag and gets 304', () => {
+        Object.assign(mockStorage, {
+          [GITHUB.LOCALSTORAGE_ETAG_KEY]: 'asdf',
+          [GITHUB.LOCALSTORAGE_PAYLOAD_KEY]: '{"idk": "mybffjill"}'
+        });
+
+        server.respondWith('GET', githubURL, [304, {}, '']);
+
+        fetchStub.resolves({
+          status: 304,
+          headers: {},
+          body: '{}'
+        });
+
+        return getActivity().then((response) => {
+          expect(response.body).to.deep.equal({ idk: 'mybffjill' });
+        });
       });
 
-      getActivity();
+      it('returns response content as json if status was 200 and passed an etag', () => {
+        Object.assign(mockStorage, {
+          [GITHUB.LOCALSTORAGE_ETAG_KEY]: 'asdf'
+        });
 
-      expect(fetchSpy).to.have.been.calledOnce;
-      expect(fetchSpy).to.have.been.calledWith(githubURL, {
-        headers: {
-          'User-Agent': GITHUB.UA,
-          ETag: 'asdf'
-        }
+        fetchStub.resolves({
+          status: 200,
+          headers: {},
+          body: '{"this": "isjson"}'
+        });
+
+        return getActivity().then((response) => {
+          expect(response.body).to.deep.equal({ this: 'isjson' });
+        });
+      });
+
+      it('memoizes the reponse body and etag after a 200 response', () => {
+        fetchStub.resolves({
+          status: 200,
+          headers: { ETag: 'foo' },
+          body: '{"most": "wonderfultimeoftheyear"}'
+        });
+
+        return getActivity().then(() => {
+          expect(mockStorage[GITHUB.LOCALSTORAGE_PAYLOAD_KEY]).to.equal('{"most": "wonderfultimeoftheyear"}');
+          expect(mockStorage[GITHUB.LOCALSTORAGE_ETAG_KEY]).to.equal('foo');
+        });
+      });
+
+      it('allows us to catch errors with a .catch()', () => {
+        fetchStub.rejects({
+          status: 500,
+          headers: {},
+          body: ''
+        });
+
+        return getActivity().catch((err) => {
+          expect(err.status).to.equal(500);
+        });
       });
     });
   });
