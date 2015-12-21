d387ecaba6a3c58deb0a2bd700b6431831c13744
Brian Jacobel <bjacobel@localytics.com>
2015-12-20 22:23:27 -0500

    They don't want us to write tests. Cloth talk 🔑  🙏

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-export const GITHUB_USER = 'bjacobel';
-export const GITHUB_UA = 'github.com/bjacobel/activity';
-export const GITHUB_LOCALSTORAGE_ETAG_KEY = 'github_events_etag';
-export const GITHUB_LOCALSTORAGE_PAYLOAD_KEY = 'github_events_payload';
+import * as github from './github';
+
+export const GITHUB = github;
--- test/support/setup.test.js
+++ test/support/setup.test.js
@@ -1,8 +1,12 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import 'sinon-as-promised';
+import sinonChai from 'sinon-chai';
+
 global.expect = expect;
 global.sinon = sinon;
+
+chai.use(sinonChai);
 
 before((done) => {
   if (global.jsdom) {
