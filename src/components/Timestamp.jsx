bd960709785b618db7b2bd9b8f5a64b8da902c17
Brian Jacobel <bjacobel@gmail.com>
2015-12-25 00:16:34 -0600

    Live updating timestamps with shouldCOmponentUpdate and setInterval. COuld be better. Don't know how to write tests for it.

--- src/components/Timestamp.jsx
+++ src/components/Timestamp.jsx
@@ -2,11 +2,35 @@ import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
 
 export default class Timestamp extends Component {
-  render() {
+  componentWillMount() {
     const { time } = this.props;
+    this.setState({ label: moment(time).fromNow() });
+  }
+
+  componentDidMount() {
+    this.ticker = global.setInterval(() => {
+      this.setState({ updated: new Date() });
+    }, 5000);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return moment(nextProps.time).fromNow() !== this.state.label;
+  }
+
+  componentWillUpdate() {
+    const { time } = this.props;
+    this.setState({ label: moment(time).fromNow() });
+  }
+
+  componentWillUnmount() {
+    global.clearInterval(this.ticker);
+  }
+
+  render() {
+    const { label } = this.state;
 
     return (
-      <p className="timestamp">{ moment(time).fromNow() }</p>
+      <p className="timestamp">{ label }</p>
     );
   }
 }
